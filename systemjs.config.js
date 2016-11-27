64cb447acde5d47d0c690eed573e6d324f4786c6
izy-izzy <izy.izzy@gmail.com>
2016-11-27 18:16:50 +0000

    fix
    fix

--- systemjs.config.js
+++ systemjs.config.js
@@ -25,8 +25,7 @@
       // other libraries
       'rxjs':                      'npm:rxjs',
       'angular-in-memory-web-api': 'npm:angular-in-memory-web-api/bundles/in-memory-web-api.umd.js',
-      'lodash' : 'npm:lodash',
-      'ng2-sweetalert2' : 'npm:ng2-sweetalert2' 
+      'lodash' : 'npm:lodash' 
     },
     // packages tells the System loader how to load when no filename and/or no extension
     packages: {
