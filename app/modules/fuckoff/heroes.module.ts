691a70474fb610e4edec276b6b97acdf8a6f3773
izy-izzy <izy.izzy@gmail.com>
2016-12-10 19:25:04 +0000

    routing refatctor.

--- app/modules/fuckoff/heroes.module.ts
+++ app/modules/fuckoff/heroes.module.ts
@@ -8,22 +8,13 @@ import { HeroesListComponent }  from './components/heroes-list.component';
 import { HeroHashPipe }         from './pipes/hero-hash.pipe';
 import { SureSaveDirective }    from './directives/sure-save.directive';
 import { HeroProviderService }  from './services/heroes-provider.service';
-
-var routes = [  
-  {path:  '',                redirectTo: '/heroeslist',          pathMatch: 'full'},
-  {path: 'heroeslist',       component: HeroesListComponent},
-  {path: 'herodetail/:uid',  component: HeroDetailComponent}
-]
+import { HeroesRouteingModule } from './routes/heroes.routes';
 
 @NgModule({
-  imports:      [ BrowserModule, 
-                  FormsModule, 
-                  RouterModule.forRoot(routes) 
-                ],
-  declarations: [ HeroesComponent, HeroDetailComponent, HeroesListComponent, HeroHashPipe, SureSaveDirective],
+  imports:      [ BrowserModule, FormsModule, HeroesRouteingModule ],
+  declarations: [ HeroesComponent, HeroDetailComponent, HeroesListComponent, HeroHashPipe, SureSaveDirective ],
   bootstrap:    [ HeroesComponent ],
-  providers:    [ HeroProviderService],
+  providers:    [ HeroProviderService ],
 })
-
 
 export class HeroesModule { }
---
+++ app/modules/fuckoff/routes/heroes.routes.ts
@@ -0,0 +1,18 @@
+import { NgModule }             from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { HeroDetailComponent }  from './../components/hero-detail.component';
+import { HeroesListComponent }  from './../components/heroes-list.component';
+
+var routes = [  
+  {path:  '',                redirectTo: '/heroeslist',          pathMatch: 'full'},
+  {path: 'heroeslist',       component: HeroesListComponent},
+  {path: 'herodetail/:uid',  component: HeroDetailComponent}
+]
+
+@NgModule({
+  imports: [ RouterModule.forRoot(routes) ],
+  exports: [ RouterModule ]
+})
+
+export class HeroesRouteingModule {}
